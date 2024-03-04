3863efc370dd9e25c76f758e541205307ef2e075
Adam Hannigan <adamhannigan81@gmail.com>
2024-03-04 11:26:32 +1100

    [CHORE] Test baord game deploy

--- src/features/portal/Portal.tsx
+++ src/features/portal/Portal.tsx
@@ -16,6 +16,7 @@ import { ChickenRescueApp } from "./examples/chickenRescue/ChickenRescue";
 initialise();
 
 export const PortalApp: React.FC = () => {
+  return <span className="text-black">TEST</span>;
   if (CONFIG.PORTAL_APP === "chicken-rescue") {
     return <ChickenRescueApp />;
   }
